3820a4763977ccff15ab5c72b97cfac8091a1762
ArmandArthur <armand.arthur@gmail.com>
2016-04-09 18:48:19 +0200

    test

--- developpement.sql
+++ developpement.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 29 Mars 2016 à 13:17
+-- Généré le :  Sam 09 Avril 2016 à 18:32
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `developpement`
 --
+CREATE DATABASE IF NOT EXISTS `developpement` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `developpement`;
 
 -- --------------------------------------------------------
 
@@ -66,8 +68,8 @@ INSERT INTO `evolution` (`id`, `personnageTypeId`, `palierInferieur`, `palierSup
 (1, 2, 0, 10, 11),
 (2, 2, 11, 20, 12),
 (3, 2, 21, 30, 9),
-(4, 1, 0, 12, 10),
-(6, 1, 12, 20, 13),
+(4, 1, 0, 10, 10),
+(6, 1, 11, 20, 13),
 (7, 1, 21, 31, 14);
 
 -- --------------------------------------------------------
@@ -81,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `iconepersonnage` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `chemin` varchar(300) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=69 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=71 ;
 
 --
 -- Contenu de la table `iconepersonnage`
@@ -107,7 +109,9 @@ INSERT INTO `iconepersonnage` (`id`, `chemin`) VALUES
 (65, 'img/personnage/road-1920-1080-1.jpg'),
 (66, 'img/personnage/road-1920-1080-1.jpg'),
 (67, 'img/personnage/road-1920-1080-5.jpg'),
-(68, 'img/personnage/road-1920-1080-3.jpg');
+(68, 'img/personnage/road-1920-1080-3.jpg'),
+(69, 'img/personnage/20140119_201412.jpg'),
+(70, 'img/personnage/20140119_202854.jpg');
 
 -- --------------------------------------------------------
 
@@ -180,38 +184,21 @@ CREATE TABLE IF NOT EXISTS `personnage` (
   `pointInvestissement` int(5) NOT NULL,
   `positionX` int(3) NOT NULL,
   `positionY` int(3) NOT NULL,
-  `tourDeJeu` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `prochainTourDeJeu` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tourDeJeu` datetime NOT NULL,
+  `prochainTourDeJeu` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `personnage`
 --
 
 INSERT INTO `personnage` (`id`, `degat`, `experience`, `mouvement`, `nom`, `nombreAttaque`, `pointDeVie`, `planId`, `pointInvestissement`, `positionX`, `positionY`, `tourDeJeu`, `prochainTourDeJeu`) VALUES
-(1, 10, 0, 6, 'lamdange', 6, 100, 1, 0, 1, 1, '2016-03-28 20:03:36', '2016-03-28 20:04:06'),
-(2, 10, 0, 0, 'lamdange2', 0, 100, 1, 0, 1, 1, '2016-03-28 20:04:01', '2016-03-28 20:04:31'),
-(3, 10, 0, 6, 'ange3', 6, 100, 1, 0, 1, 1, '2016-03-28 20:04:38', '2016-03-28 20:05:08'),
-(4, 10, 0, 5, 'choordemon1', 5, 100, 1, 0, 1, 1, '2016-03-28 20:16:28', '2016-03-28 20:16:58'),
-(5, 10, 0, 0, 'choordemon1', 0, 100, 1, 0, 4, 4, '2016-03-28 20:16:32', '2016-03-28 20:17:02'),
-(6, 10, 0, 5, 'ok', 5, 100, 1, 0, 1, 1, '2016-03-28 21:01:19', '2016-03-28 21:01:49'),
-(7, 10, 0, 5, 'ok', 5, 100, 1, 0, 1, 1, '2016-03-28 21:01:28', '2016-03-28 21:01:58'),
-(8, 10, 0, 5, 'ok', 5, 100, 1, 0, 1, 1, '2016-03-28 21:01:41', '2016-03-28 21:02:11'),
-(9, 10, 0, 5, 'ok', 5, 100, 1, 0, 1, 1, '2016-03-28 21:04:08', '2016-03-28 21:04:38'),
-(10, 10, 0, 6, 'pero', 6, 100, 1, 0, 1, 1, '2016-03-28 21:04:14', '2016-03-28 21:04:44'),
-(11, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:09:45', '2016-03-28 21:10:15'),
-(12, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:09:49', '2016-03-28 21:10:19'),
-(13, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:09:55', '2016-03-28 21:10:25'),
-(14, 10, 0, 6, 'as', 6, 100, 1, 0, 1, 1, '2016-03-28 21:11:47', '2016-03-28 21:12:17'),
-(15, 10, 0, 6, 'as', 6, 100, 1, 0, 1, 1, '2016-03-28 21:11:53', '2016-03-28 21:12:23'),
-(16, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:15:22', '2016-03-28 21:15:52'),
-(17, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:15:48', '2016-03-28 21:16:18'),
-(18, 10, 0, 6, 'as', 6, 100, 1, 0, 1, 1, '2016-03-28 21:16:28', '2016-03-28 21:16:58'),
-(19, 10, 0, 6, 'es', 6, 100, 1, 0, 1, 1, '2016-03-28 21:17:06', '2016-03-28 21:17:36'),
-(20, 10, 0, 6, 'as', 6, 100, 1, 0, 1, 1, '2016-03-28 21:37:54', '2016-03-28 21:38:24'),
-(21, 10, 0, 6, 'as', 6, 100, 1, 0, 1, 1, '2016-03-28 21:42:42', '2016-03-28 21:43:12'),
-(22, 10, 0, 6, 'mat', 6, 100, 1, 0, 1, 1, '2016-03-29 12:10:15', '2016-03-29 12:10:45');
+(1, 10, 0, 0, 'ok', 0, 100, 1, 0, 5, 5, '2016-03-29 22:21:57', '2016-03-29 22:24:27'),
+(2, 10, 0, 0, 'ko', 6, 100, 1, 0, 6, 5, '2016-03-29 22:23:32', '2016-03-29 22:26:02'),
+(3, 10, 0, 0, 'jack', 0, 100, 1, 0, 5, 3, '2016-03-29 22:23:57', '2016-03-29 22:24:27'),
+(4, 10, 0, 0, 'pm', 0, 100, 1, 0, 4, 7, '2016-03-30 11:53:37', '2016-03-30 11:56:07'),
+(5, 10, 0, 0, 'pl', 0, 100, 1, 0, 3, 7, '2016-03-30 11:55:40', '2016-03-30 11:56:10');
 
 -- --------------------------------------------------------
 
@@ -274,7 +261,7 @@ CREATE TABLE IF NOT EXISTS `_iconepersonnage_personnage` (
   `personnageId` int(11) NOT NULL,
   `iconePersonnageId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `_iconepersonnage_personnage`
@@ -283,26 +270,9 @@ CREATE TABLE IF NOT EXISTS `_iconepersonnage_personnage` (
 INSERT INTO `_iconepersonnage_personnage` (`id`, `personnageId`, `iconePersonnageId`) VALUES
 (1, 1, 11),
 (2, 2, 11),
-(3, 3, 11),
-(4, 4, 10),
-(5, 5, 10),
-(6, 6, 10),
-(7, 7, 10),
-(8, 8, 10),
-(9, 9, 10),
-(10, 10, 11),
-(11, 11, 11),
-(12, 12, 11),
-(13, 13, 11),
-(14, 14, 11),
-(15, 15, 11),
-(16, 16, 11),
-(17, 17, 11),
-(18, 18, 11),
-(19, 19, 11),
-(20, 20, 11),
-(21, 21, 11),
-(22, 22, 11);
+(3, 3, 10),
+(4, 4, 11),
+(5, 5, 11);
 
 -- --------------------------------------------------------
 
@@ -316,35 +286,18 @@ CREATE TABLE IF NOT EXISTS `_joueur_personnage` (
   `joueurId` int(5) NOT NULL,
   `personnageId` int(5) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `_joueur_personnage`
 --
 
 INSERT INTO `_joueur_personnage` (`id`, `joueurId`, `personnageId`) VALUES
-(2, 2, 1),
-(3, 2, 2),
-(4, 2, 3),
-(5, 3, 4),
-(6, 3, 5),
-(7, 2, 6),
-(8, 2, 7),
-(9, 2, 8),
-(10, 2, 9),
-(11, 2, 10),
-(12, 2, 11),
-(13, 2, 12),
-(14, 2, 13),
-(15, 2, 14),
-(16, 2, 15),
-(17, 2, 16),
-(18, 2, 17),
-(19, 2, 18),
-(20, 2, 19),
-(21, 2, 20),
-(22, 2, 21),
-(23, 2, 22);
+(1, 1, 1),
+(2, 1, 2),
+(3, 3, 3),
+(4, 2, 4),
+(5, 2, 5);
 
 -- --------------------------------------------------------
 
@@ -366,7 +319,7 @@ CREATE TABLE IF NOT EXISTS `_joueur_role` (
 INSERT INTO `_joueur_role` (`joueurId`, `roleId`) VALUES
 (1, 4),
 (2, 1),
-(3, 2),
+(3, 3),
 (4, 3),
 (5, 2);
 
@@ -424,7 +377,7 @@ CREATE TABLE IF NOT EXISTS `_personnage_personnagetype` (
   `personnageId` int(3) NOT NULL,
   `personnageTypeId` int(3) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `_personnage_personnagetype`
@@ -433,26 +386,9 @@ CREATE TABLE IF NOT EXISTS `_personnage_personnagetype` (
 INSERT INTO `_personnage_personnagetype` (`id`, `personnageId`, `personnageTypeId`) VALUES
 (1, 1, 2),
 (2, 2, 2),
-(3, 3, 2),
-(4, 4, 1),
-(5, 5, 1),
-(6, 6, 1),
-(7, 7, 1),
-(8, 8, 1),
-(9, 9, 1),
-(10, 10, 2),
-(11, 11, 2),
-(12, 12, 2),
-(13, 13, 2),
-(14, 14, 2),
-(15, 15, 2),
-(16, 16, 2),
-(17, 17, 2),
-(18, 18, 2),
-(19, 19, 2),
-(20, 20, 2),
-(21, 21, 2),
-(22, 22, 2);
+(3, 3, 1),
+(4, 4, 2),
+(5, 5, 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
